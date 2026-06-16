cfd696db80c39aaee39dd9bc97bdf09b546c1e81
Ravin Perera <rperera@novatus.global>
2026-06-16 12:24:19 +0100

    Add S3 public access block example

---
+++ terraform/security-controls/s3-public-access-block.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+resource "aws_s3_account_public_access_block" "this" {
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
